941e1fe937aae33fe04d7f42f2caa6411737a71e
Josi Reis-West <josialexa85@gmail.com>
2019-12-19 15:12:23 -0600

    Completed steps 5 through 7

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
 const {SERVER_PORT, CONNECTION_STRING} = process.env
+const pc = require('./controllers/products_controller')
 
 const app = express()
 app.use(express.json())
@@ -12,5 +13,11 @@ massive(CONNECTION_STRING)
         console.log('Connected to DB!')
     })
     .catch(err => console.log('Unable to connect', err))
+
+app.get('/api/products', pc.getAll)
+app.get('/api/products/:product_id', pc.getOne)
+app.post('/api/products', pc.create)
+app.put('/api/products/:product_id', pc.update)
+app.delete('/api/products/:product_id', pc.delete)
 
 app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
