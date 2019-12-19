aaf5bc668639f9be9881d3256b64495e91f49d3a
Josi Reis-West <josialexa85@gmail.com>
2019-12-19 14:19:53 -0600

    Completed steps 1 through 4

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const {SERVER_PORT, CONNECTION_STRING} = process.env
+
+const app = express()
+app.use(express.json())
+
+massive(CONNECTION_STRING)
+    .then(db => {
+        app.set('db', db)
+        console.log('Connected to DB!')
+    })
+    .catch(err => console.log('Unable to connect', err))
+
+app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
